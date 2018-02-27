648da0ed5c996f99151f04e7ab42a633686ec4d3
Mickael KERJEAN <mickael.kerjean@gmail.com>
2018-02-28 00:22:16 +1100

    bugfix (#17): fix service worker known issues

--- server/public/cache.js
+++ server/public/cache.js
@@ -1,79 +1,65 @@
 const CACHE_NAME = 'v0.0';
-const URLS_TO_CACHE = ['/', '/index.html'];
-const DELAY_BEFORE_SENDING_CACHE = 500;
+const DELAY_BEFORE_SENDING_CACHE = 2000;
 
+/*
+ * Control everything going through the wire, applying different
+ * strategy for caching, fetching resources
+ */
 self.addEventListener('fetch', function(event){
     if(is_a_ressource(event.request)){
-        return fetchRessource(event);
+        return event.respondWith(smartCacheStrategy(event.request));
     }else if(is_an_api_call(event.request)){
-        return fetchApi(event);
+        // TODO COOL FEATURE: https://github.com/mickael-kerjean/nuage/issues/11
+        // basically, it's all about cache invalidation sniffing inside event.request
+        if(event.request.method === "GET"){
+            if(navigator.onLine === false){
+                return event.respondWith(smartCacheStrategy(event.request));
+            }else{
+                return event.respondWith(networkFirstStrategy(event.request));
+            }
+        }
+        return event;
     }else if(is_an_index(event.request)){
-        return fetchIndex(event);
+        return event.respondWith(smartCacheStrategy(event.request))
     }else{
-        return cacheFallback(event);
+        //console.log("WTF? ", event);
+        return event;
     }
 });
 
+
+/*
+ * When a new service worker is coming in, we need to do a bit of
+ * cleanup to get rid of the rotten cache
+ */
 self.addEventListener('activate', function(event){
-    vacuum(event)
+    vacuum(event);
 });
+
+
+/*
+ * When a newly installed service worker is coming in, we want to use it
+ * straight away (make it active). By default it would be in a "waiting state"
+ */
 self.addEventListener('install', function(event){
     if (self.skipWaiting) { self.skipWaiting(); }
-})
+});
+
+
 
 ////////////////////////////////////////
-// ASSETS AND RESSOURCES
+// Test if what's the request is about
 ////////////////////////////////////////
 
 function is_a_ressource(request){
-    return ['css', 'js', 'img', 'logo', 'manifest.json', 'favicon.ico'].indexOf(pathname(request)[0]) >= 0 ? true : false;
+    return ['css', 'js', 'img', 'logo', 'manifest.json', 'favicon.ico'].indexOf(_pathname(request)[0]) >= 0 ? true : false;
 }
 
-/*
- * cache agressively but refresh the cache if possible
- */
-function fetchRessource(event){
-    if(navigator.standalone === true || navigator.onLine === false){
-        event.respondWith(cacheThenNetwork(event.request))
-    }else{
-        event.respondWith(networkThenCache(event.request));
-    }
-}
-
-
-
-////////////////////////////////////////
-// API CALL
-////////////////////////////////////////
 function is_an_api_call(request){
-    return pathname(request)[0] === 'api' ? true : false;
+    return _pathname(request)[0] === 'api' ? true : false;
 }
-
-function fetchApi(event){
-}
-
-
-
-////////////////////////////////////////
-// INDEX CALL
-////////////////////////////////////////
 function is_an_index(request){
-    return ['login', 'files', 'view', 'logout'].indexOf(pathname(request)[0]) >= 0? true : false;
-}
-function fetchIndex(event){
-    if(navigator.standalone === true || navigator.onLine === false){
-        event.respondWith(cacheThenNetwork(event.request))
-    }else{
-        event.respondWith(networkThenCache(event.request));
-    }
-}
-
-
-////////////////////////////////////////
-// OTHER STUFF
-////////////////////////////////////////
-function cacheFallback(event){
-    event.respondWith(cacheThenNetwork(event.request));
+    return ['login', 'files', 'view', 'logout'].indexOf(_pathname(request)[0]) >= 0? true : false;
 }
 
 
@@ -93,21 +79,21 @@ function vacuum(event){
     );
 }
 
-function host(request){
-    return request.url.replace(/(http[s]?\:\/\/[^\/]*\/).*/, '$1');
-}
-function pathname(request){
+function _pathname(request){
     return request.url.replace(/^http[s]?:\/\/[^\/]*\//, '').split('/')
 }
-function nil(e){}
 
-
-function cacheThenNetwork(request){
+/*
+ * Loading Strategy:
+ * use what's in cache first to make things faster but refresh it as we receive a response
+ */
+function smartCacheStrategy(request){
     return caches.open(CACHE_NAME).then(function(cache){
         return cache.match(request)
             .then(function(response){
-                if(response){
-                    fetchAndCache(request).catch(nil)
+                if(response && response.status === 200){
+                    fetchAndCache(request).catch(nil);
+                    response.headers.append('Content-Stale', 'yes');
                     return response;
                 }else{
                     return Promise.reject("OUPS");
@@ -125,7 +111,7 @@ function cacheThenNetwork(request){
         // seen on https://developers.google.com/web/fundamentals/getting-started/primers/service-workers
         return fetch(_request.clone && _request.clone() || _request)
             .then(function(response){
-                if(!response){ return response; }
+                if(!response || response.status !== 200){ return response; }
 
                 // A response is a stream and can only because we want the browser to consume the
                 // response as well as the cache consuming the response, we need to clone it
@@ -136,19 +122,28 @@ function cacheThenNetwork(request){
                 return response;
             });
     }
+    function nil(e){}
 }
 
-function networkThenCache(request){
-    let timeoutId;
 
-    return Promise.race([
-        cache(request.clone && request.clone() || request),
+// Broken as I didn't understood the Promise.race behavior correctly first ...
+// if nothing in cache it just brakes
+function networkFirstStrategy(request){
+    return new Promise(function(done, error){
+        cache(request.clone && request.clone() || request).then(function(response){
+            response.headers.append('Content-Stale', 'yes');
+            done(response);
+        });
         network(request.clone && request.clone() || request)
-    ])
+            .then(done)
+            .catch(error);
+    });
 
     function network(request){
         return fetch(request)
             .then(function(response){
+                if(!response || response.status !== 200) return Promise.reject(response);
+
                 const responseClone = response.clone();
                 caches.open(CACHE_NAME).then(function(cache){
                     cache.put(request, responseClone);
@@ -162,15 +157,13 @@ function networkThenCache(request){
 
     function cache(_request){
         return timeout()
-            .then(function(){ return caches.open(CACHE_NAME)})
-            .then(function(_cache){
-                return _cache.match(_request);
-            });
+            .then(function(){ return caches.open(CACHE_NAME); })
+            .then(function(_cache){ return _cache.match(_request); });
 
         function timeout(){
-            return new Promise(function(resolve) {
+            return new Promise(function(done) {
                 setTimeout(function() {
-                    resolve();
+                    done();
                 }, DELAY_BEFORE_SENDING_CACHE);
             });
         }
