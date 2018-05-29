8fae0a3ee6818fdc4836f4ea68aa570e974225ab
Mickael KERJEAN <mickael.kerjean@gmail.com>
2018-05-29 22:36:01 +1000

    improvement (video): improve UI of the video player

--- client/pages/viewerpage/videoplayer.js
+++ client/pages/viewerpage/videoplayer.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { MenuBar } from './menubar';
 import videojs from 'video.js';
+import 'videojs-contrib-hls';
+
 import 'video.js/dist/video-js.css';
+import './videoplayer.scss';
 
 export class VideoPlayer extends React.Component {
     constructor(props){
@@ -21,20 +24,20 @@ export class VideoPlayer extends React.Component {
 
     componentWillUnmount() {
         if (this.player) {
-            this.player.dispose()
+            this.player.dispose();
         }
     }
 
     render(){
         return (
-            <div style={{background: '#525659', height: '100%'}}>
+            <div className="component_videoplayer">
               <MenuBar title={this.props.filename} download={this.props.data} />
-              <div style={{padding: '20px'}}>
-                <div style={{maxWidth: '800px', width: '100%', margin: '0 auto'}}>
-                  <video ref={ node => this.videoNode = node } className="video-js my-skin" style={{boxShadow: 'rgba(0, 0, 0, 0.14) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 10px 0px, rgba(0, 0, 0, 0.2) 0px 2px 4px -1px'}}></video>
+              <div className="video_container">
+                <div>
+                  <video ref={ node => this.videoNode = node } className="video-js vjs-default-skin vjs-big-play-centered" style={{boxShadow: 'rgba(0, 0, 0, 0.14) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 10px 0px, rgba(0, 0, 0, 0.2) 0px 2px 4px -1px'}}></video>
                 </div>
               </div>
             </div>
-        )
+        );
     }
 }
