906fb1f3f44ccd1f4cdd5ae10942000e27f84465
Mickael KERJEAN <mickael.kerjean@gmail.com>
2019-03-07 14:31:03 +1100

    improvement (quick access): improve the user friendly aspect of the quick access bar

--- client/pages/filespage/frequently_access.js
+++ client/pages/filespage/frequently_access.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-import { Container, Icon } from '../../components/';
+import { Container, Icon, NgIf } from '../../components/';
 import { Link } from 'react-router-dom';
 import Path from 'path';
 
@@ -25,15 +25,11 @@ export class FrequentlyAccess extends React.Component {
                 .slice(0, 2);
         }
 
-        if(files.length === 0){
-            return null;
-        }
-
         return (
             <ReactCSSTransitionGroup transitionName="frequent-access" transitionLeave={false} transitionEnter={false} transitionAppear={true} transitionAppearTimeout={300}>
               <div className="component_frequently-access">
                 <Container>
-                  <span>Quick Access</span>
+                  <span className="caption">Quick Access</span>
                   <div className="frequent_wrapper">
                     {
                         files.map(function(path, index){
@@ -46,6 +42,12 @@ export class FrequentlyAccess extends React.Component {
                         })
                     }
                   </div>
+                <NgIf cond={files.length === 0} className="nothing_placeholder">
+                  <svg aria-hidden="true" focusable="false" data-icon="layer-group" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                     <path fill="currentColor" d="M12.41 148.02l232.94 105.67c6.8 3.09 14.49 3.09 21.29 0l232.94-105.67c16.55-7.51 16.55-32.52 0-40.03L266.65 2.31a25.607 25.607 0 0 0-21.29 0L12.41 107.98c-16.55 7.51-16.55 32.53 0 40.04zm487.18 88.28l-58.09-26.33-161.64 73.27c-7.56 3.43-15.59 5.17-23.86 5.17s-16.29-1.74-23.86-5.17L70.51 209.97l-58.1 26.33c-16.55 7.5-16.55 32.5 0 40l232.94 105.59c6.8 3.08 14.49 3.08 21.29 0L499.59 276.3c16.55-7.5 16.55-32.5 0-40zm0 127.8l-57.87-26.23-161.86 73.37c-7.56 3.43-15.59 5.17-23.86 5.17s-16.29-1.74-23.86-5.17L70.29 337.87 12.41 364.1c-16.55 7.5-16.55 32.5 0 40l232.94 105.59c6.8 3.08 14.49 3.08 21.29 0L499.59 404.1c16.55-7.5 16.55-32.5 0-40z"></path>
+                   </svg>
+                   Frequently access folders will be shown here
+                </NgIf>
                 </Container>
               </div>
             </ReactCSSTransitionGroup>
