ebcf3a35693f9cbc8fccaee3f00923fe89ae92f4
Mickael Kerjean <mickael@kerjean.me>
2019-05-01 02:14:10 +1000

    build (maintenance): upgrade build with latest changes

--- server/plugin/plg_image_light/install.sh
+++ server/plugin/plg_image_light/install.sh
@@ -4,5 +4,5 @@ set -e
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$(dirname "$0")"/deps
 echo `pwd`
-curl https://download.filestash.app/upload/libresize-linux-x86-64 > libresize.a
-curl https://download.filestash.app/upload/libtranscode-linux-x86-64.a > libtranscode.a
+curl https://downloads.filestash.app/upload/libresize-linux-x86-64.a > libresize.a
+curl https://downloads.filestash.app/upload/libtranscode-linux-x86-64.a > libtranscode.a
