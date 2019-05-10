9b1bad4b5c7b9119bb49f037f467e58eee4fe2c6
Mickael Kerjean <mickael@kerjean.me>
2019-05-10 15:49:40 +1000

    optimise (CSP): follow recommendations from mozilla observatory

--- server/middleware/http.go
+++ server/middleware/http.go
@@ -45,8 +45,8 @@ func IndexHeaders(fn func(App, http.ResponseWriter, *http.Request)) func(ctx App
 		header.Set("X-Content-Type-Options", "nosniff")
 		header.Set("X-XSS-Protection", "1; mode=block")
 		header.Set("X-Frame-Options", "DENY")
-		header.Set("Content-Security-Policy", "default-src 'none'; style-src 'unsafe-inline'; font-src 'self' data:; manifest-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'; object-src 'self'; media-src 'self'; worker-src 'self'; form-action 'self'")
 		header.Set("X-Powered-By", fmt.Sprintf("Filestash/%s <https://filestash.app>", APP_VERSION + "." + BUILD_NUMBER))
+		header.Set("Content-Security-Policy", "default-src 'none'; style-src 'unsafe-inline'; font-src 'self' data:; manifest-src 'self'; script-src 'self' 'sha256-JNAde5CZQqXtYRLUk8CGgyJXo6C7Zs1lXPPClLM1YM4=' 'sha256-YLOlqMUx4Lpbv7KARwKdtxDGqHGrAp77cqwdPa4A20w='; img-src 'self' data:; connect-src 'self'; object-src 'self'; media-src 'self'; worker-src 'self'; form-action 'self'; frame-ancestors 'none'; base-uri 'self'")
 		fn(ctx, res, req)
 	}
 }
