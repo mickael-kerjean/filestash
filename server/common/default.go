edf578e7103ac52ce4316eb6cc5c0ba4dd619ecf
Mickael Kerjean <mickael@kerjean.me>
2019-05-10 15:16:33 +1000

    improve (UA): set a user-agent on HTTP queries that make sense
    eg: 'Filestash/v0.4rc.20190510 (http://filestash.app)'

--- server/common/default.go
+++ server/common/default.go
@@ -1,14 +1,17 @@
 package common
 
 import (
+	"fmt"
 	"net/http"
 	"net"
 	"time"
 )
 
+var USER_AGENT = fmt.Sprintf("Filestash/%s.%s (http://filestash.app)", APP_VERSION, BUILD_NUMBER)
+
 var HTTPClient = http.Client{
 	Timeout: 5 * time.Hour,
-	Transport: &http.Transport{
+	Transport: NewTransormedTransport(http.Transport{
 		Dial: (&net.Dialer{
 			Timeout:   10 * time.Second,
 			KeepAlive: 10 * time.Second,
@@ -16,12 +19,12 @@ var HTTPClient = http.Client{
 		TLSHandshakeTimeout:   5 * time.Second,
 		IdleConnTimeout:       60 * time.Second,
 		ResponseHeaderTimeout: 60 * time.Second,
-	},
+	}),
 }
 
 var HTTP = http.Client{
 	Timeout: 10000 * time.Millisecond,
-	Transport: &http.Transport{
+	Transport: NewTransormedTransport(http.Transport{
 		Dial: (&net.Dialer{
 			Timeout:   5000 * time.Millisecond,
 			KeepAlive: 5000 * time.Millisecond,
@@ -29,5 +32,16 @@ var HTTP = http.Client{
 		TLSHandshakeTimeout:   5000 * time.Millisecond,
 		IdleConnTimeout:       5000 * time.Millisecond,
 		ResponseHeaderTimeout: 5000 * time.Millisecond,
-	},
+	}),
+}
+
+func NewTransormedTransport(transport http.Transport) http.RoundTripper {
+	return &transformedTransport{ &transport }
+}
+type transformedTransport struct {
+	original http.RoundTripper
+}
+func (this *transformedTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Add("User-Agent", USER_AGENT)
+	return this.original.RoundTrip(req)
 }
