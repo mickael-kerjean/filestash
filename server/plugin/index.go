e552b2b40d5ff87a931dc704cb87716ee629adf4
Mickael Kerjean <mickael@kerjean.me>
2019-05-17 00:40:43 +1000

    feature (console): new plugin that offer a debugging console

--- server/plugin/index.go
+++ server/plugin/index.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_mysql"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_security_scanner"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_security_svg"
+	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_handler_console"
 	. "github.com/mickael-kerjean/filestash/server/common"
 )
 
