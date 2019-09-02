b0896f0373510aa606eee7a6e52131bcfe7fdb96
Mickael Kerjean <mickael@kerjean.me>
2019-09-02 15:49:28 +1000

    maintenance (migrate): migrate the dropbox backend as a plugin

--- server/plugin/index.go
+++ server/plugin/index.go
@@ -9,6 +9,7 @@ import (
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_mysql"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_ftps"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_ldap"
+	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_dropbox"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_security_scanner"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_security_svg"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_handler_console"
