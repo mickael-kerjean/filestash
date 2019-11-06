44310fbab0ccc8480fbd5720efee8bff0bc8868f
Mickael Kerjean <mickael@kerjean.me>
2019-11-06 12:27:16 +1100

    fix (#202): fallback to in memory cache when indexedDB isn't availabe

--- client/helpers/cache.js
+++ client/helpers/cache.js
@@ -1,55 +1,61 @@
 "use strict";
 
-function Data(){
-    this.FILE_PATH = "file_path";
-    this.FILE_CONTENT = "file_content";
+const DB_VERSION = 3,
+      FILE_PATH = "file_path",
+      FILE_CONTENT = "file_content";
+
+function DataFromIndexedDB(){
     this.db = null;
-    this._init();
+    this.FILE_PATH = FILE_PATH;
+    this.FILE_CONTENT = FILE_CONTENT;
+    return this._init();
+}
+function DataFromMemory(){
+    this.data = {};
+    this.FILE_PATH = FILE_PATH;
+    this.FILE_CONTENT = FILE_CONTENT;
+    return this._init();
 }
 
-const DB_VERSION = 3;
+DataFromIndexedDB.prototype._init = function(){
+    const request = indexedDB.open("filestash", DB_VERSION);
+    request.onupgradeneeded = function(event){
+        let store;
+        let db = event.target.result;
 
-Data.prototype._init = function(){
-    const request = indexedDB.open('filestash', DB_VERSION);
-    request.onupgradeneeded = (e) => this._setup(e);
+        if(event.oldVersion == 1) {
+            // we've change the schema on v2 adding an index, let's flush
+            // to make sure everything will be fine
+            db.deleteObjectStore(FILE_PATH);
+            db.deleteObjectStore(FILE_CONTENT);
+        }else if(event.oldVersion == 2){
+            // we've change the primary key to be a (path,share)
+            db.deleteObjectStore(FILE_PATH);
+            db.deleteObjectStore(FILE_CONTENT);
+        }
+
+        store = db.createObjectStore(FILE_PATH, { keyPath: ["share", "path"] });
+        store.createIndex("idx_path", ["share", "path"], { unique: true });
+
+        store = db.createObjectStore(FILE_CONTENT, {keyPath: ["share", "path"]});
+        store.createIndex("idx_path", ["share", "path"], { unique: true });
+    };
 
     this.db = new Promise((done, err) => {
         request.onsuccess = (e) => {
             done(e.target.result);
-        }
-        request.onerror = (e) => {
-            err(e);
         };
+        request.onerror = (e) => err("INDEXEDDB_NOT_SUPPORTED");
     });
-}
-
-Data.prototype._setup = function(e){
-    let store;
-    let db = e.target.result;
-
-    if(e.oldVersion == 1) {
-        // we've change the schema on v2 adding an index, let's flush
-        // to make sure everything will be fine
-        db.deleteObjectStore(this.FILE_PATH);
-        db.deleteObjectStore(this.FILE_CONTENT);
-    }else if(e.oldVersion == 2){
-        // we've change the primary key to be a (path,share)
-        db.deleteObjectStore(this.FILE_PATH);
-        db.deleteObjectStore(this.FILE_CONTENT);
-    }
-
-    store = db.createObjectStore(this.FILE_PATH, {keyPath: ["share", "path"]});
-    store.createIndex("idx_path", ["share", "path"], { unique: true });
-
-    store = db.createObjectStore(this.FILE_CONTENT, {keyPath: ["share", "path"]});
-    store.createIndex("idx_path", ["share", "path"], { unique: true });
-}
+};
+DataFromMemory.prototype._init = function(){
+};
 
 /*
- * Fetch a record using its path, can be whether a file path or content
+ * Fetch a record using its path, can be either a file path or content
  */
-Data.prototype.get = function(type, key){
-    if(type !== this.FILE_PATH && type !== this.FILE_CONTENT) return Promise.reject({});
+DataFromIndexedDB.prototype.get = function(type, key){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
 
     return this.db.then((db) => {
         const tx = db.transaction(type, "readonly");
@@ -60,12 +66,29 @@ Data.prototype.get = function(type, key){
                 let data = query.result;
                 done(query.result || null);
             };
-            query.onerror = error;
+            query.onerror = () => done()
         });
-    }).catch(() => Promise.resolve(null));
-}
+    });
+};
+DataFromMemory.prototype.get = function(type, key){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
 
-Data.prototype.update = function(type, key, fn, exact = true){
+    const data = this.data[type] || null;
+    if(data === null){
+        return Promise.resolve(null);
+    }
+    const value = data[key.join("_")] || null;
+    if(value === null){
+        return Promise.resolve(null);
+    }
+    return new Promise((done) => {
+        requestAnimationFrame(() => done(value));
+    });
+};
+
+DataFromIndexedDB.prototype.update = function(type, key, fn, exact = true){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
+
     return this.db.then((db) => {
         const tx = db.transaction(type, "readwrite");
         const store = tx.objectStore(type);
@@ -86,11 +109,32 @@ Data.prototype.update = function(type, key, fn, exact = true){
                 cursor.continue();
             };
         });
-    }).catch(() => Promise.resolve(null));
-}
+    }).catch(() => Promise.resolve());
+};
+DataFromMemory.prototype.update = function(type, key, fn, exact = true){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
 
+    const data = this.data[type];
+    if(data === undefined){
+        return Promise.resolve(null);
+    }
 
-Data.prototype.upsert = function(type, key, fn){
+    const k = key.join("_");
+    if(exact === true){
+        if(this.data[type][k] !== undefined) this.data[type][k] = fn(data[k]);
+    }else{
+        for(let _k in data){
+            if(_k.indexOf(k) === 0){
+                this.data[type][_k] = fn(data[_k]);
+            }
+        }
+    }
+    return Promise.resolve();
+};
+
+DataFromIndexedDB.prototype.upsert = function(type, key, fn){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
+
     return this.db.then((db) => {
         const tx = db.transaction(type, "readwrite");
         const store = tx.objectStore(type);
@@ -107,10 +151,22 @@ Data.prototype.upsert = function(type, key, fn){
             query.onerror = error;
         });
     });
-}
+};
+DataFromMemory.prototype.upsert = function(type, key, fn){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
 
-Data.prototype.add = function(type, key, data){
-    if(type !== this.FILE_PATH && type !== this.FILE_CONTENT) return Promise.reject({});
+    const db = this.data[type] || null;
+    if(db === null){
+        this.data[type] = {};
+    }
+    const k = key.join("_");
+    const new_data = fn(this.data[type][k]);
+    this.data[type][k] = fn(new_data);
+    return Promise.resolve(new_data);
+};
+
+DataFromIndexedDB.prototype.add = function(type, key, data){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
 
     return this.db.then((db) => {
         return new Promise((done, error) => {
@@ -120,10 +176,21 @@ Data.prototype.add = function(type, key, data){
             request.onsuccess = () => done(data);
             request.onerror = (e) => error(e);
         });
-    }).catch(() => Promise.resolve(null));
-}
+    }).catch(() => Promise.resolve());
+};
+DataFromMemory.prototype.add = function(type, key, data){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
 
-Data.prototype.remove = function(type, key, exact = true){
+    if(this.data[type] === undefined){
+        this.data[type] = {};
+    }
+    this.data[type][key.join("_")] = data;
+    return Promise.resolve(data);
+};
+
+DataFromIndexedDB.prototype.remove = function(type, key, exact = true){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
+
     return this.db.then((db) => {
         const tx = db.transaction(type, "readwrite");
         const store = tx.objectStore(type);
@@ -152,10 +219,30 @@ Data.prototype.remove = function(type, key, exact = true){
                 };
             });
         }
-    }).catch(() => Promise.resolve(null));
-}
+    }).catch(() => Promise.resolve());
+};
+DataFromMemory.prototype.remove = function(type, key, exact = true){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
 
-Data.prototype.fetchAll = function(fn, type = this.FILE_PATH, key){
+    const data = this.data[type] || null;
+    if(data === null){
+        return Promise.resolve();
+    }
+    const k = key.join("_");
+    if(exact === true){
+        delete data[k];
+    }
+    for(let _k in data){
+        if(_k.indexOf(k) === 0){
+            delete data[_k];
+        }
+    }
+    return Promise.resolve();
+};
+
+DataFromIndexedDB.prototype.fetchAll = function(fn, type = FILE_PATH, key){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
+
     return this.db.then((db) => {
         const tx = db.transaction([type], "readonly");
         const store = tx.objectStore(type);
@@ -179,17 +266,34 @@ Data.prototype.fetchAll = function(fn, type = this.FILE_PATH, key){
             };
             request.onerror = () => {
                 done();
-            }
+            };
         });
-    }).catch(() => Promise.resolve(null));
-}
+    }).catch(() => Promise.resolve());
+};
+DataFromMemory.prototype.fetchAll = function(fn, type = FILE_PATH, key){
+    if(type !== FILE_PATH && type !== FILE_CONTENT) return Promise.reject();
 
-Data.prototype.destroy = function(){
-    clearTimeout(this.intervalId);
+    const data = this.data[type] || null;
+    if(data === null){
+        return Promise.resolve();
+    }
+    const k = key.join("_");
+    for(let _k in data){
+        if(_k.indexOf(k) === 0){
+            const ret = fn(data[_k]);
+            if(ret === false){
+                return Promise.resolve();
+            }
+        }
+    }
+    return Promise.resolve();
+};
+
+DataFromIndexedDB.prototype.destroy = function(){
     return new Promise((done, err) => {
         this.db.then((db) => {
-            purgeAll(db, this.FILE_PATH);
-            purgeAll(db, this.FILE_CONTENT);
+            purgeAll(db, FILE_PATH);
+            purgeAll(db, FILE_CONTENT);
         });
         done();
 
@@ -199,6 +303,17 @@ Data.prototype.destroy = function(){
             store.clear();
         }
     });
-}
+};
+DataFromMemory.prototype.destroy = function(){
+    this.data = {};
+    return Promise.resolve();
+};
 
-export const cache = new Data();
+export let cache = new DataFromMemory();
+if("indexedDB" in window && window.indexedDB !== null){
+    var request = indexedDB.open("_indexedDB", 1);
+    request.onsuccess = (e) => {
+        cache = new DataFromIndexedDB();
+        indexedDB.deleteDatabase("_indexedDB");
+    };
+}
