b779fe3c0e7883e3ee8ca998aa2ee4daf1313a82
Mickael Kerjean <mickael@kerjean.me>
2020-06-15 23:14:31 +1000

    feat (sorting): #214

--- client/pages/filespage.helper.js
+++ client/pages/filespage.helper.js
@@ -14,58 +14,72 @@ export const sort = function(files, type){
     }else{
         return sortByType(files);
     }
+    function _moveLoadingDownward(fileA, fileB){
+        if(fileA.icon === 'loading' && fileB.icon !== 'loading'){return +1;}
+        else if(fileA.icon !== 'loading' && fileB.icon === 'loading'){return -1;}
+        return 0;
+    };
+    function _moveFolderUpward(fileA, fileB){
+        if(['directory', 'link'].indexOf(fileA.type) === -1 && ['directory', 'link'].indexOf(fileB.type) !== -1){
+            return +1;
+        }else if(['directory', 'link'].indexOf(fileA.type) !== -1 && ['directory', 'link'].indexOf(fileB.type) === -1){
+            return -1;
+        }
+        return 0;
+    };
+    function _moveHiddenFilesDownward(fileA, fileB){
+        if(fileA.name[0] === "." && fileB.name[0] !== ".") return +1;
+        else if(fileA.name[0] !== "." && fileB.name[0] === ".") return -1;
+        return 0
+    };
     function sortByType(files){
         return files.sort((fileA, fileB) => {
-            if(fileA.icon === 'loading' && fileB.icon !== 'loading'){return +1;}
-            else if(fileA.icon !== 'loading' && fileB.icon === 'loading'){return -1;}
-            else{
-                if(['directory', 'link'].indexOf(fileA.type) === -1 && ['directory', 'link'].indexOf(fileB.type) !== -1){
-                    return +1;
-                }else if(['directory', 'link'].indexOf(fileA.type) !== -1 && ['directory', 'link'].indexOf(fileB.type) === -1){
-                    return -1;
-                }else{
-                    if(fileA.name[0] === "." && fileB.name[0] !== ".") return +1;
-                    else if(fileA.name[0] !== "." && fileB.name[0] === ".") return -1;
-                    else{
-                        const aExt = Path.extname(fileA.name.toLowerCase()),
-                              bExt = Path.extname(fileB.name.toLowerCase());
-                        if(fileA.name.toLowerCase() === fileB.name.toLowerCase()){
-                            return fileA.name > fileB.name ? +1 : -1;
-                        }else{
-                            if(aExt !== bExt) return aExt > bExt ? +1 : -1;
-                            else return fileA.name.toLowerCase() > fileB.name.toLowerCase() ? +1 : -1;
-                        }
-                    }
-                }
+            let tmp = _moveLoadingDownward(fileA, fileB);
+            if(tmp !== 0) return tmp;
+
+            tmp = _moveFolderUpward(fileA, fileB);
+            if(tmp !== 0) return tmp;
+
+            tmp = _moveHiddenFilesDownward(fileA, fileB);
+            if(tmp !== 0) return tmp;
+
+            const aExt = Path.extname(fileA.name.toLowerCase()),
+                  bExt = Path.extname(fileB.name.toLowerCase());
+
+            if(fileA.name.toLowerCase() === fileB.name.toLowerCase()){
+                return fileA.name > fileB.name ? +1 : -1;
+            }else{
+                if(aExt !== bExt) return aExt > bExt ? +1 : -1;
+                else return fileA.name.toLowerCase() > fileB.name.toLowerCase() ? +1 : -1;
             }
         });
     }
     function sortByName(files){
         return files.sort((fileA, fileB) => {
-            if(fileA.icon === 'loading' && fileB.icon !== 'loading'){return +1;}
-            else if(fileA.icon !== 'loading' && fileB.icon === 'loading'){return -1;}
-            else{
-                if(fileA.name[0] === "." && fileB.name[0] !== ".") return +1;
-                else if(fileA.name[0] !== "." && fileB.name[0] === ".") return -1;
-                else{
-                    if(fileA.name.toLowerCase() === fileB.name.toLowerCase()){
-                        return fileA.name > fileB.name ? +1 : -1;
-                    }
-                    return fileA.name.toLowerCase() > fileB.name.toLowerCase() ? +1 : -1;
-                }
+            let tmp = _moveLoadingDownward(fileA, fileB);
+            if(tmp !== 0) return tmp;
+
+            tmp = _moveFolderUpward(fileA, fileB);
+            if(tmp !== 0) return tmp;
+
+            tmp = _moveHiddenFilesDownward(fileA, fileB);
+            if(tmp !== 0) return tmp;
+
+            if(fileA.name.toLowerCase() === fileB.name.toLowerCase()){
+                return fileA.name > fileB.name ? +1 : -1;
             }
+            return fileA.name.toLowerCase() > fileB.name.toLowerCase() ? +1 : -1;
         });
     }
     function sortByDate(files){
         return files.sort((fileA, fileB) => {
-            if(fileA.icon === 'loading' && fileB.icon !== 'loading'){return +1;}
-            else if(fileA.icon !== 'loading' && fileB.icon === 'loading'){return -1;}
-            else{
-                if(fileB.time === fileA.time){
-                    return fileA.name > fileB.name ? +1 : -1;
-                }
-                return fileB.time - fileA.time;
+            let tmp = _moveLoadingDownward(fileA, fileB);
+            if(tmp !== 0) return tmp;
+
+            if(fileB.time === fileA.time){
+                return fileA.name > fileB.name ? +1 : -1;
             }
+            return fileB.time - fileA.time;
         });
     }
 };
