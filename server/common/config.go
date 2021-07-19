39f69874652e393c2ade70ec7f6c885a36d0c0aa
Mickael Kerjean <mickael@kerjean.me>
2021-07-20 00:55:23 +1000

    cleanup (#381): fit pull request

--- server/common/config.go
+++ server/common/config.go
@@ -78,7 +78,7 @@ func NewConfiguration() Configuration {
 					FormElement{Name: "display_hidden", Type: "boolean", Default: false, Description: "Should files starting with a dot be visible by default?"},
 					FormElement{Name: "auto_connect", Type: "boolean", Default: false, Description: "User don't have to click on the login button if an admin is prefilling a unique backend"},
 					FormElement{Name: "upload_button", Type: "boolean", Default: false, Description: "Display the upload button on any device"},
-					FormElement{Name: "upload_pool_size", Type: "number", Default: 10, Description: "Maximum number of files upload in parallel"},
+					FormElement{Name: "upload_pool_size", Type: "number", Default: 15, Description: "Maximum number of files upload in parallel (Default: 15)"},
 					FormElement{Name: "custom_css", Type: "long_text", Default: "", Description: "Set custom css code for your instance"},
 				},
 			},
