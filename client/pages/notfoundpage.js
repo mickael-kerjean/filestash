6e27086ac43e85405f9f29cbd7b138950397d999
Mickael Kerjean <mickael@kerjean.me>
2021-08-15 19:22:50 +1000

    maintenance (404): migrate not found page to newer react way

--- client/pages/notfoundpage.js
+++ client/pages/notfoundpage.js
@@ -1,41 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import { Button } from '../components/';
-import './error.scss';
+import { Button } from "../components/";
+import "./error.scss";
 
-export class NotFoundPage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            timeout: 10
+export function NotFoundPage({ history }) {
+    const [t, setTimer] = useState(10);
+
+    useEffect(() => {
+        const timeout = window.setTimeout(() => {
+            if (t == 0) {
+                history.push("/");
+                return
+            }
+            setTimer(t - 1);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timeout);
         };
-    }
+    }, [t]);
 
-    componentDidMount(){
-        this.countdown();
-    }
 
-    countdown(){
-        if(this.state.timeout > 0){
-            this.setState({timeout: this.state.timeout - 1});
-            window.setTimeout(() => {
-                this.countdown();
-            }, 1000);
-        }else{
-            this.props.history.push("/");
-        }
-    }
-
-    render() {
-        return (
-            <div className="component_page_notfound error-page">
-              <h1>Oops!</h1>
-              <h2>We can't seem to find the page you're looking for.</h2>
-              <p>
-                You will be redirected to the <Link to="/">homepage</Link> in {this.state.timeout} seconds
-              </p>
-            </div>
-        );
-    }
+    return (
+        <div className="component_page_notfound error-page">
+            <h1>Oops!</h1>
+            <h2>We can"t seem to find the page you"re looking for.</h2>
+            <p>
+                You will be redirected to the <Link to="/">homepage</Link> in {t} seconds
+            </p>
+        </div>
+    );
 }
