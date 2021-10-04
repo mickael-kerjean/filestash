3eb0384ce5817d05e5f14a41a83f2cbe2c000e28
Mickael Kerjean <mickael@kerjean.me>
2021-10-05 01:03:50 +1100

    fix (build): issue in build

--- server/plugin/plg_image_light/install.sh
+++ server/plugin/plg_image_light/install.sh
@@ -1,15 +1,16 @@
 #/bin/bash
 set -e
 
+echo "= INSTALL LIBS"
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$(dirname "$0")"/deps
 
-# AMD64 dependencies
-curl -s https://downloads.filestash.app/upload/libresize_Linux-x86_64.a > libresize_linux_amd64.a &
-curl -s https://downloads.filestash.app/upload/libtranscode_Linux-x86_64.a > libtranscode_linux_amd64.a &
+# AMD64
+curl -sk https://downloads.filestash.app/upload/libresize_Linux-x86_64.a > libresize_linux_amd64.a &
+curl -sk https://downloads.filestash.app/upload/libtranscode_Linux-x86_64.a > libtranscode_linux_amd64.a &
 
-# ARM dependencies
-curl -s https://downloads.filestash.app/upload/libresize_Linux-armv7l.a > libresize_linux_arm.a &
-curl -s https://downloads.filestash.app/upload/libtranscode_Linux-armv7l.a > libtranscode_linux_arm.a &
+# ARM
+curl -sk https://downloads.filestash.app/upload/libresize_Linux-armv7l.a > libresize_linux_arm.a &
+curl -sk https://downloads.filestash.app/upload/libtranscode_Linux-armv7l.a > libtranscode_linux_arm.a &
 
 wait
