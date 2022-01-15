73c23868688a58a115fbde00d34b6b735f25e031
Mickael Kerjean <mickael@kerjean.me>
2022-01-15 15:50:35 +1100

    documentation (plugin): add sample code for search plugin

--- server/plugin/plg_search_example/index.go
+++ server/plugin/plg_search_example/index.go
@@ -12,5 +12,11 @@ type ExampleSearch struct{}
 
 func (this ExampleSearch) Query(app App, path string, keyword string) ([]IFile, error) {
 	files := []IFile{}
+	files = append(files, File{
+		FName: "keyword-" + keyword + ".txt",
+		FType: "file", // ENUM("file", "directory")
+		FSize: 42,
+		FPath: "/fullpath/keyword-" + keyword + ".txt",
+	})
 	return files, nil
 }
