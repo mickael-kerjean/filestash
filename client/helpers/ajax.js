d1a2c7a3ea7819053256fea2f9b521588123bc8c
Mickael Kerjean <mickael@kerjean.me>
2022-09-02 21:07:21 +1000

    maintain (ajax): cleanup code

--- client/helpers/ajax.js
+++ client/helpers/ajax.js
@@ -4,36 +4,40 @@ export function http_get(url, type = "json", params) {
         xhr.open("GET", url, true);
         xhr.withCredentials = true;
         xhr.setRequestHeader("X-Requested-With", "XmlHttpRequest");
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    if (type === "json") {
-                        try {
-                            const data = JSON.parse(xhr.responseText);
-                            if ("status" in data === false || data.status === "ok") {
-                                done(data);
-                            } else {
-                                err(data);
-                            }
-                        } catch (error) {
-                            err({ message: "oups", trace: error });
-                        }
-                    } else {
-                        done(xhr.responseText);
-                    }
-                } else {
-                    handle_error_response(xhr, err);
-                }
-            }
-        };
-        xhr.send(null);
         xhr.onerror = function() {
             handle_error_response(xhr, err);
         };
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState !== XMLHttpRequest.DONE) {
+                return;
+            } else if (xhr.status !== 200) {
+                const isAborted = xhr.onerror === null;
+                if (isAborted) return
+                handle_error_response(xhr, err);
+                return;
+            }
+
+            if (type !== "json") {
+                done(xhr.responseText);
+                return
+            }
+            try {
+                const data = JSON.parse(xhr.responseText);
+                if ("status" in data === false || data.status === "ok") {
+                    done(data);
+                } else {
+                    err(data);
+                }
+            } catch (error) {
+                err({ message: "oups", trace: error });
+            }
+        };
+        xhr.send(null);
         if (params && params.abort) {
             params.abort.signal.onabort = () => {
+                xhr.onerror = null;
+                err({ message: "aborted", code: "ABORTED" });
                 xhr.abort();
-                handle_error_response(xhr, err);
             };
         }
     });
@@ -52,32 +56,35 @@ export function http_post(url, data, type = "json", params) {
         if (params && params.progress) {
             xhr.upload.addEventListener("progress", params.progress, false);
         }
-        xhr.send(data);
-        xhr.onload = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    try {
-                        const data = JSON.parse(xhr.responseText);
-                        if (data.status === "ok") {
-                            done(data);
-                        } else {
-                            err(data);
-                        }
-                    } catch (error) {
-                        err({ message: "oups", trace: error });
-                    }
-                } else {
-                    handle_error_response(xhr, err);
-                }
-            }
-        };
         xhr.onerror = function() {
             handle_error_response(xhr, err);
         };
+        xhr.onload = function() {
+            if (xhr.readyState !== XMLHttpRequest.DONE) {
+                return;
+            } else if (xhr.status !== 200) {
+                const isAborted = xhr.onerror === null;
+                if (isAborted) return
+                handle_error_response(xhr, err);
+                return;
+            }
+            try {
+                const data = JSON.parse(xhr.responseText);
+                if (data.status !== "ok") {
+                    err(data);
+                    return;
+                }
+                done(data);
+            } catch (error) {
+                err({ message: "oups", trace: error });
+            }
+        };
+        xhr.send(data);
         if (params && params.abort) {
             params.abort(() => {
+                xhr.onerror = null;
                 xhr.abort();
-                err({ message: "aborted" });
+                err({ message: "aborted", code: "ABORTED" });
             });
         }
     });
@@ -89,26 +96,26 @@ export function http_delete(url) {
         xhr.open("DELETE", url, true);
         xhr.withCredentials = true;
         xhr.setRequestHeader("X-Requested-With", "XmlHttpRequest");
-        xhr.onload = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    try {
-                        const data = JSON.parse(xhr.responseText);
-                        if (data.status === "ok") {
-                            done(data);
-                        } else {
-                            err(data);
-                        }
-                    } catch (error) {
-                        err({ message: "oups", trace: error });
-                    }
-                } else {
-                    handle_error_response(xhr, err);
-                }
-            }
-        };
         xhr.onerror = function() {
             handle_error_response(xhr, err);
+        };
+        xhr.onload = function() {
+            if (xhr.readyState !== XMLHttpRequest.DONE) {
+                return;
+            } else if (xhr.status !== 200) {
+                handle_error_response(xhr, err);
+                return;
+            }
+            try {
+                const data = JSON.parse(xhr.responseText);
+                if (data.status !== "ok") {
+                    err(data);
+                    return
+                }
+                done(data);
+            } catch (error) {
+                err({ message: "oups", trace: error });
+            }
         };
         xhr.send(null);
     });
@@ -120,22 +127,25 @@ export function http_options(url) {
         xhr.open("OPTIONS", url, true);
         xhr.withCredentials = true;
         xhr.setRequestHeader("X-Requested-With", "XmlHttpRequest");
+        xhr.onerror = function() {
+            handle_error_response(xhr, err);
+        };
         xhr.onload = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status !== 200) {
-                    handle_error_response(xhr, err);
-                    return;
-                }
-                done(
-                    xhr.getAllResponseHeaders()
-                        .split("\n")
-                        .reduce((acc, r) => {
-                            const a = r.split(": ");
-                            acc[a[0]] = a[1];
-                            return acc;
-                        }, {}),
-                );
+            if (xhr.readyState !== XMLHttpRequest.DONE) {
+                return
+            } else if (xhr.status !== 200) {
+                handle_error_response(xhr, err);
+                return;
             }
+            done(
+                xhr.getAllResponseHeaders()
+                    .split("\n")
+                    .reduce((acc, r) => {
+                        const a = r.split(": ");
+                        acc[a[0]] = a[1];
+                        return acc;
+                    }, {}),
+            );
         };
         xhr.send(null);
     });
@@ -157,38 +167,43 @@ function handle_error_response(xhr, err) {
 
     if (navigator.onLine === false) {
         err({ message: "Connection Lost", code: "NO_INTERNET" });
-    } else if (xhr.status === 0 && xhr.responseText === "") {
-        switch(xhr.readyState) {
-        case XMLHttpRequest.DONE:
-        case XMLHttpRequest.UNSENT:
-            err({ message: "aborted", code: "ABORTED" });
-            break
-        default:
-            err({
-                message: "Service unavailable, if the problem persist, contact your administrator",
-                code: "INTERNAL_SERVER_ERROR",
-            });
-        }
-    } else if (xhr.status === 500) {
+        return;
+    }
+    switch(xhr.status) {
+    case 500:
         err({
             message: message || "Oups something went wrong with our servers",
             code: "INTERNAL_SERVER_ERROR",
         });
-    } else if (xhr.status === 401) {
+        break;
+    case 401:
         err({ message: message || "Authentication error", code: "Unauthorized" });
-    } else if (xhr.status === 403) {
+        break;
+    case 403:
         err({ message: message || "You can\'t do that", code: "Forbidden" });
-    } else if (xhr.status === 413) {
+        break;
+    case 413:
         err({ message: message || "Payload too large", code: "PAYLOAD_TOO_LARGE" });
-    } else if (xhr.status === 502) {
+    case 502:
         err({ message: message || "The destination is acting weird", code: "BAD_GATEWAY" });
-    } else if (xhr.status === 409) {
+    case 409:
         if (response["error_summary"]) { // dropbox way to say doesn't exist
             err({ message: "Doesn\'t exist", code: "UNKNOWN_PATH" });
-        } else {
-            err({ message: message || "Oups you just ran into a conflict", code: "CONFLICT" });
+            return
         }
-    } else {
+        err({ message: message || "Oups you just ran into a conflict", code: "CONFLICT" });
+    case 0:
+        switch(xhr.responseText) {
+        case "":
+            err({
+                message: "Service unavailable, if the problem persist, contact your administrator",
+                code: "INTERNAL_SERVER_ERROR",
+            });
+            break;
+        default:
+            err({ message: xhr.responseText, code: "INTERNAL_SERVER_ERROR" });
+        }
+    default:
         err({ message: message || "Oups something went wrong" });
     }
 }
