69b0b71ce5cc56eadb7a229912aae0a0003fbd96
Mickael Kerjean <mickael@kerjean.me>
2023-02-09 08:09:17 +1100

    feature (title): set title to general.name in static pages

--- server/common/response.go
+++ server/common/response.go
@@ -132,6 +132,7 @@ func Page(stuff string) string {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>` + Config.Get("general.name").String() + `</title>
     <style>
       html { background: #f2f3f5; font-size: 16px; font-family: "San Francisco","Roboto","Arial",sans-serif; }
       body { text-align: center; padding-top: 50px; text-align: center; margin: 0; }
