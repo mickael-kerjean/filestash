40f575037c7723953ec6e65352fd62a32690e05d
MickaelK <mickael@kerjean.me>
2023-10-20 20:05:35 +1100

    chore (cleanup): cleanup public folder

--- public/boot/ctrl_boot_frontoffice.test.js
+++
@@ -1,9 +0,0 @@
-import exec from "./ctrl_boot_backoffice.js";
-
-describe("ctrl::boot", () => {
-    it("runs", async() => {
-        const start = new Date();
-        await exec();
-        expect(new Date() - start).toBeLessThan(100);
-    });
-});
