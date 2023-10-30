3c379d50ee9f24054791695916e157e43f673d3b
Stéphane Lam <stelam@gmail.com>
2023-10-30 17:43:47 -0400

    fix (api): api key validation

--- server/common/api.go
+++ server/common/api.go
@@ -8,20 +8,20 @@ import (
 
 func VerifyApiKey(api_key string) (host string, err error) {
 	isApiEnabled := Config.Get("features.api.enable").Bool()
-	apiKey := Config.Get("feature.api.api_key").String()
+	allowedApiKeys := Config.Get("features.api.api_key").String()
 
 	if isApiEnabled == false {
 		return "", NewError("Api is not enabled", 503)
-	} else if apiKey == os.Getenv("API_KEY") {
+	} else if api_key == os.Getenv("API_KEY") {
 		return "*", nil
 	}
-	lines := strings.Split(apiKey, "\n")
+	lines := strings.Split(allowedApiKeys, "\n")
 	for _, line := range lines {
 		line = regexp.MustCompile(` #.*`).ReplaceAllString(line, "") // remove comment
 		chunks := strings.SplitN(line, " ", 2)
 		if len(chunks) == 0 {
 			continue
-		} else if chunks[0] != apiKey {
+		} else if chunks[0] != api_key {
 			continue
 		}
 		if len(chunks) == 1 {
