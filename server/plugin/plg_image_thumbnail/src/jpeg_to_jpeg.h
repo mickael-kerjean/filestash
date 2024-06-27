f4fe61c512b6be1ae2e41273c4b15a5c71949700
MickaelK <mickael@kerjean.me>
2024-06-27 23:43:51 +1000

    chore (maintenance): deprecate old thumbnail plugin

--- server/plugin/plg_image_thumbnail/src/jpeg_to_jpeg.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include "jpeglib.h"
-#include "utils.h"
-
-#define JPEG_QUALITY 50
--- server/plugin/plg_image_thumbnail/src/main_png_to_webp.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "image_png.h"
-
-int main(int argc, char **argv) {
-  return png_to_webp(stdin, stdout, 200);
-}
