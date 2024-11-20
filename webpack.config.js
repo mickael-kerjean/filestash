0da11bf8cda9363020a55f602148ce27a8ccc453
MickaelK <mickael@kerjean.me>
2024-11-20 16:39:47 +1100

    fix (webpack): another webpack issue

--- webpack.config.js
+++ webpack.config.js
@@ -91,6 +91,7 @@ const config = {
             "crypto": require.resolve("crypto-browserify"),
             "buffer": require.resolve("buffer/"),
             "stream": require.resolve("stream-browserify"),
+            "vm": require.resolve("vm-browserify"),
         },
     },
     mode: process.env.NODE_ENV || "production",
