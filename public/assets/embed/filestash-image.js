9332c3b9ef45893befd6056ac99a0aad25cef7b5
MickaelK <mickael@kerjean.me>
2025-06-17 18:38:26 +1000

    fix (embed): image embedding iframe policy

--- public/assets/embed/filestash-image.js
+++ public/assets/embed/filestash-image.js
@@ -12,6 +12,7 @@ class FilestashImage extends HTMLElement {
         this.iframe = document.createElement("iframe");
         this.iframe.setAttribute("style", "width: 100%; height: 100%; border: none; display: block;");
         this.iframe.setAttribute("sandbox", "allow-downloads allow-scripts allow-presentation allow-forms allow-same-origin");
+        this.iframe.setAttribute("referrerpolicy", "origin");
         this.shadowRoot.appendChild(this.iframe);
 
         this.debounce = null;
