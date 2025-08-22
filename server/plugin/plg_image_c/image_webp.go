da731c17e8cf85e25d2aef637c75696ebeae8ed3
MickaelK <mickael@kerjean.me>
2025-08-22 16:24:58 +1000

    chore (maintenance): thumbnailing for webp

--- server/plugin/plg_image_c/image_webp.go
+++ server/plugin/plg_image_c/image_webp.go
@@ -1,7 +1,7 @@
 package plg_image_c
 
 // #include "image_webp.h"
-// #cgo LDFLAGS: -l:libwebp.a
+// #cgo LDFLAGS: -l:libwebp.a -lsharpyuv
 import "C"
 
 func webp(input uintptr, output uintptr, size int) {
