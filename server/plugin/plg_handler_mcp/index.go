4aef4fea4e460e735fecfc5973fb111d79e2a4f0
Mickael <mickael@kerjean.me>
2025-09-05 13:41:17 +1000

    fix (plg_handler_mcp): maintain mcp - #875

--- server/plugin/plg_handler_mcp/index.go
+++ server/plugin/plg_handler_mcp/index.go
@@ -26,17 +26,16 @@ func init() {
 			return nil
 		}
 		srv := Server{}
-		r.HandleFunc("/sse", srv.sseHandler)
-		r.HandleFunc("/messages", srv.messageHandler)
-		r.HandleFunc("/.well-known/oauth-authorization-server", srv.WellKnownInfoHandler)
-		r.HandleFunc("/mcp/authorize", srv.AuthorizeHandler)
-		r.HandleFunc("/mcp/token", srv.TokenHandler)
-		r.HandleFunc("/mcp/register", srv.RegisterHandler)
-		r.HandleFunc("/api/mcp", NewMiddlewareChain(
-			srv.CallbackHandler,
-			[]Middleware{SessionStart, LoggedInOnly},
-			*app,
-		))
+		m := []Middleware{}
+		r.HandleFunc("/sse", NewMiddlewareChain(srv.sseHandler, m, *app))
+		r.HandleFunc("/messages", NewMiddlewareChain(srv.messageHandler, m, *app))
+		r.HandleFunc("/.well-known/oauth-authorization-server", NewMiddlewareChain(srv.WellKnownInfoHandler, m, *app))
+		r.HandleFunc("/mcp/authorize", NewMiddlewareChain(srv.AuthorizeHandler, m, *app))
+		r.HandleFunc("/mcp/token", NewMiddlewareChain(srv.TokenHandler, m, *app))
+		m = []Middleware{BodyParser}
+		r.HandleFunc("/mcp/register", NewMiddlewareChain(srv.RegisterHandler, m, *app))
+		m = []Middleware{SessionStart, LoggedInOnly}
+		r.HandleFunc("/api/mcp", NewMiddlewareChain(srv.CallbackHandler, m, *app))
 		return nil
 	})
 }
