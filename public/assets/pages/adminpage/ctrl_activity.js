79aaf52dbd7eeda832209c082ac5bc915367bf68
MickaelK <mickael@kerjean.me>
2025-10-07 15:52:16 +1100

    feature (admin): activity graph

--- public/assets/pages/adminpage/ctrl_activity.js
+++ public/assets/pages/adminpage/ctrl_activity.js
@@ -3,6 +3,7 @@ import { createElement, createRender } from "../../lib/skeleton/index.js";
 import { initConfig } from "./model_config.js";
 import componentLogForm from "./ctrl_activity_form.js";
 import componentLogViewer from "./ctrl_activity_viewer.js";
+import componentLogGraph from "./ctrl_activity_graph.js";
 import componentAuditor from "./ctrl_activity_audit.js";
 import transition from "./animate.js";
 import AdminHOC from "./decorator.js";
@@ -10,11 +11,12 @@ import AdminHOC from "./decorator.js";
 export default AdminHOC(async function(render) {
     const $page = createElement(`
         <div class="component_logpage sticky">
-            <h2>Events</h2>
+            <h2>System Logs</h2>
             <div class="component_logviewer"></div>
+            <div class="component_stats"></div>
             <div class="component_logger"></div>
 
-            <h2>Activity Report</h2>
+            <h2>Audit Report</h2>
             <div class="component_audit"></div>
         <div>
     `);
@@ -23,5 +25,6 @@ export default AdminHOC(async function(render) {
 
     componentLogViewer(createRender($page.querySelector(".component_logviewer")));
     componentLogForm(createRender($page.querySelector(".component_logger")));
+    componentLogGraph(createRender($page.querySelector(".component_stats")));
     componentAuditor(createRender($page.querySelector(".component_audit")));
 });
