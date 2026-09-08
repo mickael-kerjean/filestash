94a310e8f0938bd4da440a1b4d465c459f6be3d7
MickaelK <mickael@kerjean.me>
2026-09-09 00:38:27 +1000

    fix (plg/extension): rust sdk form issue

--- server/pkg/extension/sdk/rust/src/form.rs
+++ server/pkg/extension/sdk/rust/src/form.rs
@@ -3,6 +3,6 @@ use serde::Serialize;
 #[derive(Default, Serialize)]
 pub struct Form {
     pub label: String,
-    pub kind: String,
+    #[serde(rename = "type")] pub kind: String,
     pub value: Option<String>,
 }
