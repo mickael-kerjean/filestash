f49acb7ba1cdaa500a78b1f705d1d68904bc67af
MickaelK <mickael@kerjean.me>
2026-09-10 17:15:34 +1000

    fix (window): avoid linux only syscall

--- server/pkg/config/utils.go
+++ server/pkg/config/utils.go
@@ -3,7 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
-	"syscall"
+	"reflect"
 	"time"
 
 	. "github.com/mickael-kerjean/filestash/server/pkg/env"
@@ -16,24 +16,32 @@ func assertFS(path string) {
 		fmt.Printf("FATAL ERROR - %v\n", err)
 		os.Exit(1)
 	}
-	stat, ok := info.Sys().(*syscall.Stat_t)
-	if !ok {
+	uid, gid, mode := os.Getuid(), os.Getgid(), info.Mode().Perm()
+	if uid == -1 {
 		return
 	}
-	uid, gid, mode := os.Getuid(), os.Getgid(), info.Mode().Perm()
+	stat := reflect.Indirect(reflect.ValueOf(info.Sys()))
+	if stat.Kind() != reflect.Struct {
+		return
+	}
+	owner, group := stat.FieldByName("Uid"), stat.FieldByName("Gid")
+	if !owner.CanUint() || !group.CanUint() {
+		return
+	}
+	ownerUID, ownerGID := int(owner.Uint()), int(group.Uint())
 	if uid == 0 {
 		return
 	} else if mode&0002 != 0 {
 		return
-	} else if mode&0200 != 0 && uid == int(stat.Uid) {
+	} else if mode&0200 != 0 && uid == ownerUID {
 		return
 	} else if mode&0020 != 0 {
-		if gid == int(stat.Gid) {
+		if gid == ownerGID {
 			return
 		}
 		if groups, err := os.Getgroups(); err == nil {
 			for _, g := range groups {
-				if g == int(stat.Gid) {
+				if g == ownerGID {
 					return
 				}
 			}
@@ -47,7 +55,7 @@ func assertFS(path string) {
  HOW TO FIX    chown -R %d:%d %s
 =================================================================================
  shutting down ...
-`, APPNAME, uid, stat.Uid, uid, gid, GetAbsolutePath(""))
+`, APPNAME, uid, ownerUID, uid, gid, GetAbsolutePath(""))
 	time.Sleep(5 * time.Second)
 	os.Exit(1)
 }
